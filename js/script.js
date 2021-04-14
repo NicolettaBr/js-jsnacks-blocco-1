b98c71e20f251f1aafb459e556f98244ec84ecb2
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-04-14 16:23:36 +0200

    corretto con funzione somma al posto di somme varie

--- js/script.js
+++ js/script.js
@@ -59,19 +59,9 @@ var zucchine = [
 
 //console.log(zucchine);
 
-var sommaPeso = 0;
 
-for (var i = 0; i < zucchine.length; i++){
-    thisZucchina = zucchine[i];
-
-    var pesoZucchina = thisZucchina.peso_gr;
-    //console.log(thisZucchina.peso_gr);
-
-    sommaPeso = sommaPeso + pesoZucchina;
-    
-};
-
-console.log('peso totale zucchine:', sommaPeso);
+var pesoTotaleZucchine = sommaZucchine(zucchine);
+console.log(pesoTotaleZucchine);
 
 
 //ES.2
@@ -153,29 +143,32 @@ for( var i = 0; i < zucchine.length; i++){
     }
 };
 
-var sommaPesoMag = 0;
-
-for ( var j = 0; j < arrayMaggiore15.length; j++){
-    thisObjectMag = arrayMaggiore15[j];
-
-    var pesoMag = thisObjectMag.peso_gr;
-    //console.log(thisObjectMag.peso_gr);
-
-    sommaPesoMag = sommaPesoMag + pesoMag;
-
-};
-console.log('peso tot arrayMag15:', sommaPesoMag);
+var sommaPesoMag = sommaZucchine(arrayMaggiore15);
+console.log(sommaPesoMag);
 
 
-var sommaPesoMin = 0;
+var sommaPesoMin = sommaZucchine(arrayMinore15);
+console.log(sommaPesoMin)
 
-for ( var k = 0; k < arrayMinore15.length; k++){
-    thisObjectMin = arrayMinore15[k];
 
-    var pesoMin = thisObjectMin.peso_gr;
-    //console.log(thisObjectMin.peso_gr);
 
-    sommaPesoMin = sommaPesoMin + pesoMin;
 
-};
-console.log('peso tot arrayMin15:', sommaPesoMin);
+//funzione che somma il peso delle zucchine in un array
+
+function sommaZucchine (arrayZucchine){
+    var sommaPeso = 0;
+
+    for (var i = 0; i < arrayZucchine.length; i++){
+        thisZucchina = arrayZucchine[i];
+
+        var pesoZucchina = thisZucchina.peso_gr;
+        //console.log(thisZucchina.peso_gr);
+
+        sommaPeso = sommaPeso + pesoZucchina;
+        
+    };
+
+    //console.log('peso totale zucchine:', sommaPeso);
+    return sommaPeso;
+
+}
